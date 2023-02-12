ab1be18cc0531703b816ceda1c6d96750e743c06
Blueonn33 <91683317+Blueonn33@users.noreply.github.com>
2023-02-12 16:40:33 +0200

    added a modal

--- Mansor/ClientApp/src/components/TaskGroupsComponent/TaskGroupsComponent.js
+++ Mansor/ClientApp/src/components/TaskGroupsComponent/TaskGroupsComponent.js
@@ -1,13 +1,50 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Component } from 'react'
+import { endpoints } from '../../endpoints';
+import { AddTaskGroup } from '../AddTaskGroup/AddTaskGroup';
 import '../TaskGroupsComponent/TaskGroupsComponent.css';
+import TaskGroupsContainer from '../TaskGroupsContainer/TaskGroupsContainer';
 
-export class TaskGroupsComponent extends Component {
-    static displayName = TaskGroupsComponent.name;
+export default class TaskGroupsComponent extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = { taskGroups: [] }
+        this.loadTaskGroups = this.loadTaskGroups.bind(this);
+    }
+    async componentDidMount() {
+        this.loadTaskGroups();
+    }
+    async loadTaskGroups() {
+        await fetch(endpoints.loadTaskGroups(), {
+            mode: 'no-cors'
+        })
+            .then((res) => res.json())
+            .then((res) => this.setState({ taskGroups: res }))
+    }
     render() {
         return (
-            <div>
+            <div className='taskGroupsListWrapper d-flex justify-content-center align-items-center'>
+                <h1>Groups </h1>
+                <div className='taskGroupsContainer'>
+                    <div className='taskGroupsContent'>
+                        <div className='taskGroupsListHeaderWrapper d-flex'>
+                            <h4 className='taskGroupsListHeader'>Groups</h4>
+                        </div>
+                        <div className='createNewTaskGroupButtonWrapper'>
+                            <AddTaskGroup onTaskGroupAdded={this.loadTaskGroups} />
+                        </div>
+                        <div className='taskGroupsContainer'>
+                            {this.state.taskGroups.map((taskGroup) => {
+                                return (
+                                    <TaskGroupsContainer taskGroupData={taskGroup} key={taskGroup.id} />
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
         );
     }
 }
