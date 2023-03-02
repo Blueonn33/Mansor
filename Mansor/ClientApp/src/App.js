9d4356025344d83228c6b39dd67aa43f1dec8b44
Blueonn33 <91683317+Blueonn33@users.noreply.github.com>
2023-03-02 18:08:27 +0200

    added some things

--- Mansor/ClientApp/src/App.js
+++ Mansor/ClientApp/src/App.js
@@ -1,6 +1,5 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import AppRoutes from './AppRoutes';
-import { RegisterMenu } from './components/api-authorization/RegisterMenu';
 import { LandingPage } from './components/LandingPage/LandingPage';
 import { Layout } from './components/Layout';
 import { Route, Routes } from "react-router-dom";
@@ -9,57 +8,29 @@ import TaskGroupsComponent from './components/TaskGroupsComponent/TaskGroupsComp
 import { Notes } from './components/Notes/Notes';
 import TasksComponent from './components/TasksComponent/TasksComponent';
 import { TasksCalendar } from './components/TasksCalendar/TasksCalendar';
+import { RegisterMenu } from './components/api-authorization/RegisterMenu';
 
-//export default class App extends Component {
-//    static displayName = App.name;
+export default class App extends Component {
+    static displayName = App.name;
 
-//    render() {
-//        return (
-//            <Layout>
-//                <Routes>
-//                    {/*{AppRoutes.map((route, index) => {*/}
-//                    {/*  const { element, ...rest } = route;*/}
-//                    {/*  return <Route key={index} {...rest} element={element} />;*/}
-//                    {/*})}*/}
+    render() {
+        return (
+            <Layout>
+                <Routes>
+                    {/*{AppRoutes.map((route, index) => {*/}
+                    {/*  const { element, ...rest } = route;*/}
+                    {/*  return <Route key={index} {...rest} element={element} />;*/}
+                    {/*})}*/}
 
-//                    <Route path='/' element={<LandingPage />} />
-//                    <Route path='/register' element={<RegisterMenu />} />
-//                    {/*   <Route path='/taskGroups/:id' element={<Tasks />} />*/}
-//                    <Route path='/taskItems/:id' element={<TasksComponent />} />
-//                    <Route path='/taskGroups' element={<TaskGroupsComponent />} />
-//                    <Route path='/calendar' element={<TasksCalendar />} />
-//                    <Route path='/notes' element={<Notes />} />
-//                </Routes>
-//            </Layout>
-//        );
-//    }
-//}
-
-
-function App() {
-    const [token, setToken] = useState();
-
-    if (!token) {
-        return <RegisterMenu setToken={setToken} />
+                    <Route path='/' element={<LandingPage />} />
+                    <Route path='/register' element={<RegisterMenu />} />
+                    {/*   <Route path='/taskGroups/:id' element={<Tasks />} />*/}
+                    <Route path='/taskItems/:id' element={<TasksComponent />} />
+                    <Route path='/taskGroups' element={<TaskGroupsComponent />} />
+                    <Route path='/calendar' element={<TasksCalendar />} />
+                    <Route path='/notes' element={<Notes />} />
+                </Routes>
+            </Layout>
+        );
     }
-    return (
-        <Layout>
-            <Routes>
-                {/*{AppRoutes.map((route, index) => {*/}
-                {/*  const { element, ...rest } = route;*/}
-                {/*  return <Route key={index} {...rest} element={element} />;*/}
-                {/*})}*/}
-
-                <Route path='/' element={<LandingPage />} />
-                <Route path='/register' element={<RegisterMenu />} />
-                {/*   <Route path='/taskGroups/:id' element={<Tasks />} />*/}
-                <Route path='/taskItems/:id' element={<TasksComponent />} />
-                <Route path='/taskGroups' element={<TaskGroupsComponent />} />
-                <Route path='/calendar' element={<TasksCalendar />} />
-                <Route path='/notes' element={<Notes />} />
-            </Routes>
-        </Layout>
-    );
 }
-
-export default App;
